cdf5e87c6ae0c56dfafe0de77af3a429dc936378
unknown <walid.bomb@gmail.com>
2021-11-21 17:01:53 +0000

    Controller middleware
    Moved the auth middleware from api.php to ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     /**
+     * Protected routes ['store', 'update', 'destroy']
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum', ['except' => ['index', 'show', 'search']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
--- routes/api.php
+++ routes/api.php
@@ -16,26 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::resource('products', ProductController::class);
-
-// Public routes
+//-------------------------Auth routes-----------------------------------------
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Products routes
+Route::apiResource('products', ProductController::class);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
-
-
-// Protected routes
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
