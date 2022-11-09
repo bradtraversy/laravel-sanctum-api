b9602b90d1d34505f7a66302a9a2bee7f02ed1b4
Mobina <nimobina99@gmail.com>
2022-11-09 17:20:50 +0330

    add HasApiTokens trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable , HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
